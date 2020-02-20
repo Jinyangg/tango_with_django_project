1f5e1e0707573a0d382db8c808094ef05f89e365
Jinyangg <574772854@qq.com>
2020-02-20 15:05:36 +0000

    'chapter5_updata'

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
@@ -8,6 +9,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
